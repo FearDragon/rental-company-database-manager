8f7ec43a25fccc3d43b71608e7e1764b336b9a39
FearDragon <coolpixfb@gmail.com>
2026-07-02 16:25:40 -0400

    Added the ability to enter entries into houses table

--- src/main/java/com/github/feardragon/rental_company_database_manager/FXMLController.java
+++ src/main/java/com/github/feardragon/rental_company_database_manager/FXMLController.java
@@ -6,7 +6,11 @@ import java.sql.Date;
 import java.sql.SQLException;
 import java.util.ResourceBundle;
 import java.util.function.UnaryOperator;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -97,8 +101,33 @@ public class FXMLController implements Initializable {
     private DatePicker incomeDateDue;
 
     // Enter Houses
+    ObservableList<String> states = FXCollections.observableArrayList(
+            "AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", "GA",
+            "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD",
+            "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ",
+            "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC",
+            "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY");
     @FXML
     private AnchorPane enterHousesMenu;
+    @FXML
+    private ComboBox<String> housesStateComboBox;
+    @FXML
+    private TextField zipCodeField;
+    @FXML
+    private TextField housesEmailField;
+    @FXML
+    private TextField housesPhoneField;
+    @FXML
+    private TextField housesStreetField;
+    @FXML
+    private TextField housesCityField;
+    @FXML
+    private TextField housesCountyField;
+    @FXML
+    private TextField housesFirstNameField;
+    @FXML
+    private TextField housesLastNameField;
+
 
     private Expenses expenses;
     private Income income;
@@ -121,6 +150,8 @@ public class FXMLController implements Initializable {
     public void setOptions() throws SQLException {
         expenseHouseComboBox.getItems().addAll(houses.getAddresses());
         incomeHouseComboBox.getItems().addAll(houses.getAddresses());
+        housesStateComboBox.getItems().addAll(states);
+
     }
 
     @Override
@@ -154,8 +185,27 @@ public class FXMLController implements Initializable {
             String newText = change.getControlNewText();
             return newText.matches("^\\d*(\\.\\d{0,2})?$") ? change : null;
         };
+        // Formats zip code field
+        UnaryOperator<TextFormatter.Change> zipFilter = change -> {
+            String newText = change.getControlNewText();
+            return newText.matches("^\\d{0,5}(-\\d{0,4})?$") ? change : null;
+        };
+        // Formats phone number field
+        UnaryOperator<TextFormatter.Change> phoneFilter = change -> {
+            String newText = change.getControlNewText();
+            return newText.matches("^\\+?[0-9\\s\\-]*$") ? change : null;
+        };
         setupMoneyField(expensePriceField, moneyFilter);
         setupMoneyField(incomeAmountField, moneyFilter);
+        setupPhoneField(housesPhoneField, phoneFilter);
+        zipCodeField.setTextFormatter(new TextFormatter(zipFilter));
+        housesEmailField.focusedProperty().addListener((observable, oldValue, newValue) -> {
+            if (!newValue) {
+                if(!housesEmailField.getText().contains("@") || !housesEmailField.getText().contains(".")) {
+                    System.out.println("Please enter a valid email address");
+                }
+            }
+        });
     }
 
     public void handleExpensePriceAction() {
@@ -166,6 +216,10 @@ public class FXMLController implements Initializable {
         formatMoneyField(incomeAmountField);
     }
 
+    public void handleHousesPhoneAction() {
+        formatPhoneField(housesPhoneField);
+    }
+
     // Sets up the format filter the money field and adds 0's to the end when clicking off the field
     private void setupMoneyField(TextField field, UnaryOperator<TextFormatter.Change> filter) {
         field.setTextFormatter(new TextFormatter<>(filter));
@@ -174,6 +228,56 @@ public class FXMLController implements Initializable {
                 formatMoneyField(field);
             }
         });
+    }
+    // Sets up the phone number field and formats it by sectioning the number and adding "-" and "+"
+    private void setupPhoneField(TextField field, UnaryOperator<TextFormatter.Change> filter) {
+        field.setTextFormatter(new TextFormatter<>(filter));
+        field.focusedProperty().addListener((observable, oldValue, newValue) -> {
+            if (!newValue) {
+                formatPhoneField(field);
+            }
+        });
+    }
+
+    // Formats phone number by adding "-" and "+" for sectioning and country code
+    private void formatPhoneField(TextField field) {
+        String text = field.getText().trim();
+        if (text.isEmpty()) {
+            return;
+        }
+
+        String cleanText = text.replaceAll("[^0-9+]", "");
+        String digitsOnly = text.replaceAll("[^0-9]", "");
+        String prefix = "";
+        String localDigits = cleanText;
+
+        if (digitsOnly.length() > 10) {
+            int prefixLength = cleanText.length() - 10;
+            prefix = cleanText.substring(0, prefixLength).trim();
+            localDigits = cleanText.substring(prefixLength);
+            prefix = prefix + " ";
+            if (!prefix.startsWith("+") && !prefix.isEmpty()) {
+                prefix = "+" +  prefix;
+            }
+        } else if (cleanText.startsWith("+")) {
+            field.setText(cleanText);
+            prefix = prefix + " ";
+            return;
+        }
+        int length = localDigits.length();
+        String formattedNumber;
+
+        if (length <= 3) {
+            formattedNumber = localDigits;
+        } else if (length <= 6) {
+            formattedNumber = localDigits.substring(0, 3) + "-" + localDigits.substring(3);
+        } else {
+            formattedNumber = localDigits.substring(0, 3) + "-" +
+                    localDigits.substring(3, 6) + "-" +
+                    localDigits.substring(6);
+        }
+
+        field.setText(prefix + formattedNumber);
     }
 
     // Adds 0's to end of money amount
@@ -240,6 +344,7 @@ public class FXMLController implements Initializable {
             expensePriceField.getText().isBlank() ||
             expenseDateField.getValue() == null) {
             System.out.println("Please fill all the fields");
+            return;
         }
         houseID = expenseHouseComboBox.getSelectionModel().getSelectedIndex() + 1;
         expenseName = expenseNameField.getText();
@@ -268,6 +373,7 @@ public class FXMLController implements Initializable {
                 expensePriceField.getText().isBlank() ||
                 expenseDateField.getValue() == null) {
             System.out.println("Please fill all the fields");
+            return;
         }
         houseID = incomeHouseComboBox.getSelectionModel().getSelectedIndex() + 1;
         incomeName = incomeNameField.getText();
@@ -281,6 +387,54 @@ public class FXMLController implements Initializable {
         System.out.println("Selected date due: " + dateDue);
         try{
             incomeTable.getItems().add(income.enterEntry(houseID, incomeName, incomeAmount, datePaid, dateDue));
+            System.out.println("Successfully entered entry");
+        } catch(Exception e){
+            System.out.println("Failed to enter");
+        }
+    }
+
+    public void enterHouse(ActionEvent event){
+        String streetAddress;
+        String city;
+        String county;
+        String state;
+        String zipCode;
+        String firstName;
+        String lastName;
+        String email;
+        String phoneNumber;
+        if (housesStreetField.getText().isBlank() ||
+            housesCityField.getText().isBlank() ||
+            housesCountyField.getText().isBlank() ||
+            housesStateComboBox.getSelectionModel().getSelectedIndex() == -1 ||
+            zipCodeField.getText().isBlank() ||
+            housesFirstNameField.getText().isBlank() ||
+            housesLastNameField.getText().isBlank() ||
+            housesEmailField.getText().isBlank() ||
+            housesPhoneField.getText().isBlank()) {
+            System.out.println("Please fill all the fields");
+            return;
+        }
+        streetAddress = housesStreetField.getText();
+        city = housesCityField.getText();
+        county = housesCountyField.getText();
+        state = housesStateComboBox.getSelectionModel().getSelectedItem();
+        zipCode = zipCodeField.getText();
+        firstName = housesFirstNameField.getText();
+        lastName = housesLastNameField.getText();
+        email = housesEmailField.getText();
+        phoneNumber = housesPhoneField.getText();
+        System.out.println("Selected street: " + streetAddress);
+        System.out.println("Selected city: " + city);
+        System.out.println("Selected county: " + county);
+        System.out.println("Selected state: " + state);
+        System.out.println("Selected zip code: " + zipCode);
+        System.out.println("Selected first name: " + firstName);
+        System.out.println("Selected last name: " + lastName);
+        System.out.println("Selected email: " + email);
+        System.out.println("Selected phone number: " + phoneNumber);
+        try {
+            housesTable.getItems().add(houses.enterEntry(streetAddress, city, county, state, zipCode, firstName, lastName, email, phoneNumber));
             System.out.println("Successfully entered entry");
         } catch(Exception e){
             System.out.println("Failed to enter");
--- src/main/java/com/github/feardragon/rental_company_database_manager/Houses.java
+++ src/main/java/com/github/feardragon/rental_company_database_manager/Houses.java
@@ -4,17 +4,84 @@ import com.zaxxer.hikari.HikariDataSource;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.math.BigDecimal;
+import java.sql.*;
 
 public class Houses {
     private final static String selectTable = "SELECT * FROM houses";
+    private static final String selectByID = "SELECT * FROM houses WHERE House_id = ?";
+    private static final String insertEntry = "INSERT INTO houses (StreetAddress, City, County, State, ZipCode, FirstName, LastName, Email, PhoneNumber) values (?, ?, ?, ?, ?, ?, ?, ?, ?)";
+    private static final String getLastEntry = "SELECT * FROM houses ORDER BY House_id DESC LIMIT 1";
     HikariDataSource dataSource;
 
     public Houses(HikariDataSource dataSource) {
         this.dataSource = dataSource;
+    }
+
+    // Returns HouseRow based on income_ID
+    public HouseRow getEntryByID (int id) throws SQLException{
+        int houseID;
+        String streetAddress;
+        String city;
+        String county;
+        String state;
+        String zipCode;
+        String firstName;
+        String lastName;
+        String email;
+        String phoneNumber;
+        HouseRow result;
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(selectByID)){
+            pstmt.setInt(1, id);
+            try (ResultSet rs = pstmt.executeQuery()){
+                rs.next();
+                houseID = rs.getInt(1);
+                streetAddress = rs.getString(2);
+                city = rs.getString(3);
+                county = rs.getString(4);
+                state = rs.getString(5);
+                zipCode = rs.getString(6);
+                firstName = rs.getString(7);
+                lastName = rs.getString(8);
+                email = rs.getString(9);
+                phoneNumber = rs.getString(10);
+                result = new HouseRow(houseID, streetAddress, city, county, state, zipCode, firstName, lastName, email, phoneNumber);
+                return result;
+            }
+        }
+    }
+
+    public int getLastID() throws SQLException{
+        int houseID = -1;
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(getLastEntry)){
+            try (ResultSet rs = pstmt.executeQuery()) {
+                if (rs.next()) {
+                    houseID = rs.getInt(1);
+                }
+            }
+        }
+        return houseID;
+    }
+
+    public HouseRow enterEntry(String streetAddress, String city, String county, String state, String zipCode, String firstName, String lastName, String email, String phoneNumber) throws SQLException{
+        HouseRow result;
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(insertEntry)) {
+            pstmt.setString(1, streetAddress);
+            pstmt.setString(2, city);
+            pstmt.setString(3, county);
+            pstmt.setString(4, state);
+            pstmt.setString(5, zipCode);
+            pstmt.setString(6, firstName);
+            pstmt.setString(7, lastName);
+            pstmt.setString(8, email);
+            pstmt.setString(9, phoneNumber);
+            pstmt.executeUpdate();
+            result = getEntryByID(getLastID());
+        }
+        return result;
     }
 
     public ObservableList<HouseRow> getTable() throws SQLException {
